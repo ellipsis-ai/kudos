9f29884961da221031ed95d7db7918ff7d719021
Luke Andrews <luke@attaboy.ca>
2018-02-16 21:49:01 +0000

    v1

---
+++ actions/kudos/function.js
@@ -0,0 +1,47 @@
+function(name, ellipsis) {
+  const EllipsisApi = require('ellipsis-api');
+const storageApi = new EllipsisApi(ellipsis).storage;
+
+const giver = ellipsis.userInfo.userName;
+console.log(giver);
+const recipient = name.replace(/^@/, "");
+storageApi.query({
+  query: `
+mutation GiveKudos($kudos: KudosInput!) {
+  createKudos(kudos: $kudos) {
+    id
+    recipient
+    giver
+    timestamp
+  }
+}
+`,
+  variables: {
+    kudos: {
+      recipient: recipient,
+      giver: giver,
+      timestamp: Date.now()
+    }
+  }
+}).then((result) => {
+  return storageApi.query({
+    query: `
+query MeasureKudos($filter: KudosInput!) {
+  kudosList(filter: $filter) {
+    recipient
+    timestamp
+  }
+}`, variables: {
+      filter: {
+        recipient: recipient
+      }
+    }
+  });
+}).then((result) => {
+  ellipsis.success({
+    giver: giver,
+    recipient: recipient,
+    count: result.data.kudosList.length
+  });
+});
+}
